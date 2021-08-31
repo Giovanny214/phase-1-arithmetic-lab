1e5388828628b7b3e0b61d29ba16c1a925b7ea0c
Giovanny <gioggonzalez214@gmail.com>
2021-08-31 16:54:29 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let multiply = 62; 
+let num1 = 31
+let num2 = 2 ;{
+    num1 * num2 === multiply
+}
 
-// Write your code here
+let random = 1 
+ Math.floor(Math.random() + random);
+
+let mod = 4
+let num3 = 12
+let num4 = 3; {
+    num3 / num4 === mod
+}
+
+let max = 20
+Math.max(1, 12, 4, 18, 20)
